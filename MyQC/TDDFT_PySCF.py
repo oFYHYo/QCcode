18f24923aeddb047595fb3c33237bd6dc1e330f7
oFYHYo <3357044257@qq.com>
2024-11-21 17:19:03 +0800

    add os and plot

--- MyQC/TDDFT_PySCF.py
+++ MyQC/TDDFT_PySCF.py
@@ -1,22 +1,31 @@
 import numpy as np
-from pyscf import gto,dft,tdscf
+from pyscf import gto,dft,tdscf,lib
 import time
+import matplotlib.pyplot as plt
+from math import log,exp,pi
+
 
 class TDA:
     
-    def __init__(self,mf_hf,mol):
-
-        td = tdscf.TDA(mf_hf)
-        A,_ = td.get_ab()
-
+    def __init__(self,mf_hf):
+        
         self.num_eig = 6
+        self.mol = mf_hf.mol
         self.nocc =  int(mol.nelectron)//2
         self.mo_ene = mf_hf.mo_energy
-        self.A_size = A.shape[0]*A.shape[1]
+        self.mo_coeff = mf_hf.mo_coeff
+        self.e = None
+        self.xy = None
+        self.os = None
+        self.FWHM = None
+    
+    def gen_vind(self):
         
-
-        TD_matrix = A
-        self.TD_matrix = TD_matrix.reshape(self.A_size,self.A_size)
+        td = tdscf.TDA(mf_hf)
+        vind, _ = td.gen_vind()
+        self.vind = vind
+        return vind
+        
 
     def gen_hdiag(self):
         nocc = self.nocc
@@ -33,9 +42,10 @@ class TDA:
         
         hdiag = self.gen_hdiag()
         k = self.num_eig
-        TD_matrix = self.TD_matrix
 
-        A_size = TD_matrix.shape[0]
+        vind = self.gen_vind()
+
+        A_size = self.nocc*(self.mo_ene.shape[0]-self.nocc)
 
         eig = 2*k
 
@@ -47,12 +57,19 @@ class TDA:
             V_old[Dsort[j], j] = 1.0
 
         max_iter = 50
+        
+        print('iter       sub_size       |r|_max')
         for i in range(max_iter):
             
             # 获得子空间下的A矩阵
-            W_old = np.einsum("ki,il->kl",TD_matrix,V_old)
+            #W_old = np.einsum("ki,il->kl",TD_matrix,V_old)
+            W_old = vind(V_old.T).T
             sub_A = np.einsum("ik,il->kl",V_old,W_old)
             
+            upper = np.triu_indices(n=sub_A.shape[0], k=1)
+            lower = (upper[1], upper[0])
+            sub_A[lower] = sub_A[upper]
+
             val,ket = np.linalg.eigh(sub_A)
             
             sub_val = val[:k]
@@ -64,7 +81,8 @@ class TDA:
             r_norm = np.linalg.norm(residual,axis=0).tolist()
            
             max_norm = np.max(r_norm)
-            if i>2 and max_norm < 1e-8:
+            print(f'{i+1:3d}{sub_A.shape[1]:12}{max_norm:18.6f}')
+            if i>2 and max_norm < 1e-6:
                 y = time.time()
                 print(f'Davidson对角化完成，耗时{y-x:.6f}s')
                 break
@@ -79,35 +97,119 @@ class TDA:
             V_new = np.hstack([V_old,new_guess])
             V_old,_ = np.linalg.qr(V_new)
 
-        return sub_val
+        self.e = sub_val[:k]
+        self.xy = np.einsum("ki,il->kl",V_old,sub_ket)*np.sqrt(.5)
+        
+        return sub_val 
     
-    def kernel(self):
+    def oscillator_strength(self):
 
+        nocc = self.nocc
+        
+        mo_c = self.mo_coeff
+        XY = self.xy
+        X = XY.reshape(nocc,mo_c.shape[0]-nocc,-1)
+        mol = self.mol
+
+        # ao基下的偶极矩
+        u_ao = mol.intor_symmetric('int1e_r', comp=3)
+
+        # 计算基态到各激发态的跃迁偶极矩
+        # Phys. Chem. Chem. Phys., 2020, 22, 26838.
+        u_mo = lib.einsum("ua,xuv,vi->xia",mo_c[:,nocc:],u_ao,mo_c[:,:nocc])
+        u_trans = lib.einsum('iak,xia->xk',X,u_mo)*2
+    
+        # 计算振子强度
+        u2 = (u_trans**2).sum(0)
+        f = 2*self.e*(u2)/3
+
+        self.os = f
+        print(self.os)
+
+        return f
+
+    def plot(self,type='eV', xrange=None):
+
+        if type == 'eV':
+            label = 'Excited energy(eV)'
+            ene = self.e*self.Hartree_to_eV
+            if self.FWHM == None:
+                FWHM = 0.66667
+            else:
+                FWHM = self.FWHM
+        
+        elif type == 'nm':
+            label = 'Wavelength(nm)'
+            ene = 1240.7011/(self.e*self.Hartree_to_eV)
+            if self.FWHM == None:
+                FWHM = 100
+            else:
+                FWHM = self.FWHM
+
+        if xrange == None:
+            ma = np.max(ene)
+            mi = np.min(ene)
+            L = (ma-mi)/8
+            xrange = [mi-L,ma+L]
+ 
+        x = np.arange(xrange[0],xrange[1],(xrange[1]-xrange[0])/10000)
+        y = np.zeros_like(x)
+        for i in range(self.num_eig):
+            yi = self.Gau(x,ene[i],FWHM)
+            yi = yi*self.os[i]
+            y += yi
+            plt.plot(x,yi)
+            plt.bar(ene[i],self.os[i],width=0.01)
+
+        plt.plot(x,y)
+        plt.xlabel(label)
+        plt.ylabel('Oscillator_Strength')
+        plt.title('UV-Vis')
+        plt.show()
+
+    
+    def Gau(self,x,x0,FWHM):
+        c = FWHM/(2*np.sqrt(2*np.log(2)))
+        z = np.exp(-(x-x0)**2/(2*(c**2)))
+        return z/(c*np.sqrt(2*pi))
+    
+    def Lor(self,x,ene):
+        y = 0
+        for i in range(self.num_eig):
+            y += self.os[i]/((x-ene[i])**2+0.25*self.FWHM**2)
+        return y*self.FWHM/(2*pi)
+    
+    def PV(self,x,ene,weight=0.5):
+        return weight*self.Gau(x,ene)+(1-weight)*self.Lor(x,ene)
+
+    def kernel(self):
+        print('开始对角化')
         val = self.Davidson()
-        Hartree_to_eV = 27.211385050
-        print(val*Hartree_to_eV)
+        self.Hartree_to_eV = 27.211385050
+        print('Excited energy(eV)')
+        print(val*self.Hartree_to_eV)
 
 if __name__ == '__main__':
     mol = gto.Mole()
     mol.atom = '''
-    C                 -0.71205900   -0.00001100    0.00164200
-    C                 -0.02242200    1.21633600    0.00195500
-    C                  1.36411500    1.21608600    0.00308400
-    C                  2.08781200   -0.00001200    0.00431800
-    C                  1.36414600   -1.21610700    0.00338000
-    C                 -0.02240200   -1.21632400    0.00226400
-    H                 -0.59131100    2.14539300   -0.00013100
-    H                  1.90773300    2.16404900    0.00493600
-    H                  1.90769600   -2.16410100    0.00542100
-    H                 -0.59129100   -2.14538500    0.00041900
-    N                  3.45592600    0.00001600    0.03587500
-    H                  3.95679500    0.85870800   -0.14709400
-    H                  3.95687100   -0.85864300   -0.14710100
-    N                 -2.17251000   -0.00000200   -0.00193800
-    O                 -2.74286400   -1.08297000   -0.00375000
-    O                 -2.74283000    1.08298000   -0.00298300
+    C                  0.70961700    0.00001300    0.00280400
+    C                  0.02236000    1.21539400    0.00395200
+    C                 -1.36207000    1.21443500    0.00511500
+    C                 -2.07956400    0.00001000    0.00585600
+    C                 -1.36204100   -1.21441300    0.00526800
+    C                  0.02237800   -1.21537900    0.00409700
+    H                  0.58456400    2.14068200    0.00122300
+    H                 -1.90417300    2.15610600    0.00960600
+    H                 -1.90421700   -2.15604300    0.00981500
+    H                  0.58455100   -2.14068800    0.00145800
+    N                  2.16356300   -0.00000400   -0.00311100
+    O                  2.74121800    1.09085300   -0.00539300
+    O                  2.74116600   -1.09087600   -0.00578500
+    N                 -3.45821100    0.00000000    0.05558900
+    H                 -3.93071600    0.84590900   -0.23149700
+    H                 -3.93063400   -0.84610300   -0.23107900
     '''
-    mol.basis = '6-31G*'
+    mol.basis = 'sto-3g'
     mol.build()
 
     Func = 'PBE'
@@ -116,23 +218,16 @@ if __name__ == '__main__':
     mf_hf.xc = Func
     mf_hf.kernel()
 
-    # PySCF
+    td = TDA(mf_hf)
+    td.num_eig =  6
+    td.kernel()
+
+    #td.FWHM=100
+    os = td.oscillator_strength()
+    td.plot('eV')
+'''
     td = tdscf.TDA(mf_hf)
-    print('PySCF所得激发能')
-    td.nstates=6
+    td.nstates = 6
     td.kernel()
-    
-    # Davidson对角化
-    td = TDA(mf_hf,mol)
-    td.kernel()
-    print()
-
-    # numpy的eigh函数
-    A = td.TD_matrix
-    x = time.time()
-    Hartree_to_eV = 27.211385050
-    val,ket = np.linalg.eigh(A)
-    y = time.time()
-
-    print(f"eigh计算完成，耗时{y-x:.6f}s")
-    print(f'{val[:6]*Hartree_to_eV}')
+    td.analyze()
+'''
--- temp.py
+++ temp.py
@@ -1,19 +1,89 @@
 import numpy as np
-from pyscf import gto,dft,tdscf
+from pyscf import gto,dft,tdscf,lib
 import time
-
+from functools import partial
+np.einsum = partial(np.einsum,optimize=["greedy", 1024 ** 3 * 40 / 8])
 mol = gto.Mole()
 mol.atom = '''
-C                 -0.66295800    0.00000000   -0.00000000
-C                  0.66295800    0.00000000   -0.00000000
-H                 -1.25654334    0.92403753    0.00000000
-H                 -1.25654334   -0.92403753    0.00000000
-H                  1.25654334   -0.92403753    0.00000000
-H                  1.25654334    0.92403753   -0.00000000
+ C                  0.69091000   -0.00000900    0.00005600
+ C                  0.02087600    1.23401200    0.11547300
+ C                 -1.35141400    1.24115600    0.12060300
+ C                 -2.06641700    0.00002400   -0.00004200
+ C                 -1.35141700   -1.24115400   -0.12043100
+ C                  0.02086700   -1.23402500   -0.11525900
+ H                  0.59903700    2.14730700    0.19073200
+ H                 -1.90322200    2.17148500    0.21249700
+ H                 -1.90326700   -2.17146900   -0.21223300
+ H                  0.59900600   -2.14734500   -0.19037800
+ N                  2.16510300    0.00002700   -0.00006700
+ O                  2.70857600   -1.04975800    0.32016200
+ O                  2.70858700    1.04973800   -0.32029700
+ N                 -3.39959000    0.00001100   -0.00024900
+ H                 -3.93397400    0.86093900    0.07829100
+ H                 -3.93390500   -0.86104600   -0.07801600
 '''
-mol.basis = '6-31G*'
+mol.charge = 1
+mol.spin = 1
+mol.basis = 'sto-3g'
 mol.build()
+int1e_r = mol.intor_symmetric('int1e_r', comp=3)
 
+mf = dft.UKS(mol)
+mf.xc = 'PBE'
+mf.kernel()
+td = tdscf.TDA(mf)
+td.kernel()
+xy = td.xy[0][0]
+dipole = td.transition_dipole()
+'''
+dm = mf.make_rdm1()[0]
+print(dm.shape)
+dipole = np.einsum('xuv,uv',int1e_r,dm)
+'''
+mol.atom = '''
+ C                  1.69091000   -0.00000900    0.00005600
+ C                  1.02087600    1.23401200    0.11547300
+ C                 -0.35141400    1.24115600    0.12060300
+ C                 -1.06641700    0.00002400   -0.00004200
+ C                 -0.35141700   -1.24115400   -0.12043100
+ C                  1.02086700   -1.23402500   -0.11525900
+ H                  1.59903700    2.14730700    0.19073200
+ H                 -0.90322200    2.17148500    0.21249700
+ H                 -0.90326700   -2.17146900   -0.21223300
+ H                  1.59900600   -2.14734500   -0.19037800
+ N                  3.16510300    0.00002700   -0.00006700
+ O                  3.70857600   -1.04975800    0.32016200
+ O                  3.70858700    1.04973800   -0.32029700
+ N                 -2.39959000    0.00001100   -0.00024900
+ H                 -2.93397400    0.86093900    0.07829100
+ H                 -2.93390500   -0.86104600   -0.07801600
+'''
+mol.build()
+int1e_r1 = mol.intor_symmetric('int1e_r', comp=3)
+print((int1e_r-int1e_r1)[0])
+
+mf1 = dft.UKS(mol)
+mf1.xc = 'PBE'
+mf1.kernel()
+td1 = tdscf.TDA(mf1)
+td1.kernel()
+xy1 = td1.xy[0][0]
+dipole1 = td1.transition_dipole()
+os1 = td1.oscillator_strength()
+os = td.oscillator_strength()
+print(os1-os)
+'''
+dm1 = mf.make_rdm1()[0]
+dipole1 = np.einsum('xuv,uv',int1e_r1,dm)
+'''
+#print((dipole-dipole1))
+#print(xy1[1]-xy[1])
+#print(np.allclose(dm,dm1))
+
+
+#print(np.allclose(int1e_r,int1e_r1))
+
+'''
 Func = 'PBE'
 
 mf_hf = dft.RKS(mol)
@@ -24,76 +94,91 @@ td = tdscf.TDA(mf_hf)
 print('PySCF所得激发能')
 td.nstates=6
 td.kernel()
+os = td.oscillator_strength()
+e = td.e*27.211385050
+#print(e)
+#print(os)
 
-A,B = td.get_ab()
+dipole = td.transition_dipole()
+print(dipole)
+print(mol.atom_coords)
+for i in range(mol.atom_coords):
+    print(i)
 
-A_size = A.shape[0]*A.shape[1]
-x = time.time()
-# TD_matrix = np.einsum('iakl,kljb->iajb',A+B,A-B)
-TD_matrix = A
-TD_matrix = TD_matrix.reshape(A_size,A_size)
+
+
+from math import pi
+import matplotlib.pyplot as plt
+def Gau(x,x0,FWHM):
+    c = FWHM/(2*np.sqrt(2*np.log(2)))
+    y = np.exp(-(x-x0)**2/(2*(c**2)))
+    return y/(c*np.sqrt(2*pi))
+x = np.arange(2,5,3/1000)
+y = np.zeros_like(x)
+for i in range(len(td.e)):
+    yi = Gau(x,e[i],0.6667)*os[i]
+    y += yi
+    plt.plot(x,yi)
+    plt.bar(e[i],os[i],width=0.01)
+plt.plot(x,y)
+plt.show()
+
 
 mo_ene = mf_hf.mo_energy
 nocc = int(mol.nelectron)//2
 e_ia = (mo_ene[nocc:].reshape(-1,1) - mo_ene[:nocc]).T
 hdiag = e_ia.ravel()
 
-# vind,hdiag = td.gen_vind()
+os = td.oscillator_strength()
+print('PySCF_os')
+print(os)
+X = td.xy
 
-# V = np.eye(A_size,A_size)
-# TD_matrix = np.array(vind(V))
+mo_c = mf_hf.mo_coeff
 
-k = 6
-eig = 2*k
-V_old = np.zeros([A_size,eig])
-hdiag = hdiag.reshape(-1,)
-Dsort = hdiag.argsort()
-for j in range(k):
-    V_old[Dsort[j], j] = 1.0
+grids = dft.gen_grid.Grids(mol)
+grids.build()
+#weights = grids.weights
+#ni = dft.numint.NumInt()
+#ao_1 = ni.eval_ao(mol,grids.coords,deriv=1)
+#ao_0 = ao_1[0]
+#coords = grids.coords
 
-max_iter = 50
-for i in range(max_iter):
-    
-    W_old = np.einsum("ki,il->kl",TD_matrix,V_old)
-    
-    sub_A = np.einsum("ik,il->kl",V_old,W_old)
-    
-    val,ket = np.linalg.eigh(sub_A)
-    
-    sub_val = val[:k]
-    sub_ket = ket[:,:k]
+u = np.zeros([len(X),3])
+Dm_0 = np.diag(mf_hf.mo_occ)
+transdip = td.transition_dipole()
+-0.00578500
+int1e_r = mol.intor_symmetric('int1e_r', comp=3)
 
-    residual = np.einsum("ki,il->kl",W_old,sub_ket) - np.einsum("l,ki,il->kl",sub_val,V_old,sub_ket)
-    #print(np.linalg.norm(W_old))
-    r_norm = np.linalg.norm(residual,axis=0).tolist()
-    #print(r_norm)
-    max_norm = np.max(r_norm)
-    if i>2 and max_norm < 1e-8:
-        y = time.time()
-        break
+#Dm_0_ao = np.einsum('pi,ij,qj->pq',mo_c,Dm_0,mo_c.conj())
 
-    t = 1e-8
-    D = np.repeat(hdiag.reshape(-1,1), k, axis=1) - sub_val
-    D = np.where( abs(D) < t, np.sign(D)*t, D)
-    new_guess = residual/D
+#Dm = np.diag(mf_hf.mo_occ)
 
-    V_new = np.hstack([V_old,new_guess])
-    V_old,_ = np.linalg.qr(V_new)
+#Dm_ao = np.einsum('pi,ij,qj->pq',mo_c,Dm,mo_c.conj())
 
-Hartree_to_eV = 27.211385050
+#rho_0 = np.einsum("gu,gv,uv->g",ao_0,ao_0,Dm_0_ao)
 
-print(f'Davidson:{sub_val*Hartree_to_eV}，耗时{y-x:.6f}')
+#r_e = np.einsum("kx,k,k->x",coords,rho_0,weights)
+#r_e /= np.einsum('k,k',rho_0,weights)
 
-x1 = time.time()
-val,_ = np.linalg.eigh(TD_matrix)
-y1 = time.time()
-print(f'numpy.eigh:{val[:k]*Hartree_to_eV}，耗时{y1-x1:.6f}')
+#u_ao = np.einsum("gu,gv,gx,g->xuv",ao_0,ao_0,coords,weights)
+#print(np.linalg.norm(u_ao-int1e_r))
 
-X_ia = np.zeros([A_size,k])
-for i in range(k):
-    eqn = TD_matrix - sub_val[i]*np.eye(A_size)
-    r = np.zeros(A_size)
-    X_ia[:,i] = np.linalg.solve(eqn,r)
-print(r.shape)
-X_ia = X_ia.reshape(A.shape[0],A.shape[1],-1)
-print(np.linalg.solve(eqn,r))
+for i in range(len(X)):
+
+    u_mo = lib.einsum("ua,xuv,vi->xia",mo_c[:,nocc:],int1e_r,mo_c[:,:nocc])
+    #u_mo -= lib.einsum('k,k->',rho_0**2,weights)
+    u_k = lib.einsum('ia,xia->x',X[i][0],u_mo)*2
+    u[i] = u_k
+
+u2 = (u**2).sum(1)
+print('my_os')
+f = 2*td.e*(u2)/3
+
+#print(transdip)
+print(f)
+
+
+vind,_ = td.gen_vind()
+A = np.ones([8*28,4])
+print(vind(A.T).T.shape)'''
